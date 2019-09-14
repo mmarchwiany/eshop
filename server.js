02024f2c54eae3c43bdce836b0c44de9fca7f0be
mmarchwiany <marcin.marchwiany@gmail.com>
2019-09-14 23:44:52 +0800

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -13,6 +14,7 @@ db.on("error", error => console.error(error));
 // db.on("open", () => console.log("Connected to database"));
 
 app.use(express.json());
+app.use(cors());
 
 const gamesRouter = require("./routes/games");
 const pricesRouter = require("./routes/prices");
