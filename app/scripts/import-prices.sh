8f508033d98725828bd875929f0982d919795091
mmarchwiany <marcin.marchwiany@gmail.com>
2019-09-30 22:27:37 +0800

    scripts fix

--- app/scripts/import-prices.sh
+++ app/scripts/import-prices.sh
@@ -1,4 +1,8 @@
-for m in GB, PL, AT, SE, NO, CZ, ZA, LT, LV, PT, SI, SK, DE, RO, BE, BG, EE, ES, FR, FI, IE, IT, LU, MT, NL, CY, HR, HU, DK, RU, CH; do
-    for i in {1..50}; do node ./commands/prices-importer.js -p $i -m $m; done;
-    sleep 1m;
+for m in PL GB AT SE NO CZ ZA LT LV PT SI SK DE RO BE BG EE ES FR FI IE IT LU MT NL CY HR HU DK RU CH; do
+    echo "Start importing prices for country: $m"
+    for i in {1..50}; do 
+        node ./commands/prices-importer.js -p $i -m $m; 
+    done;
+
+    sleep 5s;
 done;
