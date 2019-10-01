b99d694b1cd96694b281720ec8a42cb289dcd955
mmarchwiany <marcin.marchwiany@bemyguest.com.sg>
2019-10-01 10:36:37 +0800

    scripts improvements

--- app/scripts/import-prices.sh
+++ app/scripts/import-prices.sh
@@ -1,8 +1,8 @@
 for m in PL GB AT SE NO CZ ZA LT LV PT SI SK DE RO BE BG EE ES FR FI IE IT LU MT NL CY HR HU DK RU CH; do
-    echo "Start importing prices for country: $m"
+    echo  "[`date +"%F %H:%M:%S"`] Start importing prices for country: $m"
     for i in {1..50}; do 
         node ./commands/prices-importer.js -p $i -m $m; 
     done;
 
-    sleep 5s;
+    sleep 1m;
 done;
