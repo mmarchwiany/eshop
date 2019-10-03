05a1e485a03611b9f0430a41f25941a102ba8aa3
mmarchwiany <marcin.marchwiany@bemyguest.com.sg>
2019-10-03 15:43:40 +0800

    move logs

--- app/scripts/import-prices.sh
+++ app/scripts/import-prices.sh
@@ -1,6 +1,6 @@
 for m in PL GB AT SE NO CZ ZA LT LV PT SI SK DE RO BE BG EE ES FR FI IE IT LU MT NL CY HR HU DK RU CH; do
     echo  "[`date +"%F %H:%M:%S"`] Start importing prices for country: $m"
     for i in {1..50}; do 
-        node ./commands/prices-importer.js -p $i -m $m >> prices-importer.log; 
+        node ./commands/prices-importer.js -p $i -m $m >> ../storage/logs/prices-importer.log; 
     done;
 done;
